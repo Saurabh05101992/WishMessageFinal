4633c348ab63c89a33dba31739545d1064f46dd7
saurabh <saurabh304323@gmail.com>
2019-05-15 10:57:26 +0530

    all proj commited

---
+++ SpringMVCProj2-WishMessage/src/main/java/com/nt/controller/WishMessageGenerator.java
@@ -0,0 +1,30 @@
+package com.nt.controller;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class WishMessageGenerator extends AbstractController {
+
+	@Override
+	public ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res)
+			throws Exception {
+		
+		int hour =0;
+		Calendar calendar = null;
+		calendar = Calendar.getInstance();
+		String msg=null;
+		
+		hour = calendar.get(Calendar.HOUR_OF_DAY);
+		
+		if(hour >12)
+			msg="Good night";
+		
+		return new ModelAndView("wishmessage","msg",msg);
+	}
+
+}
